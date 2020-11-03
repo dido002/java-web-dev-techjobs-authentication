b8b4e2740814e7952743780e2768ea7c67a275e7
Umer <an01985i@gmail.com>
2020-11-02 21:14:50 -0600

    studio 19

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/dto/LoginFormDTO.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/dto/LoginFormDTO.java
@@ -1,2 +1,48 @@
-package org.launchcode.javawebdevtechjobsauthentication.models.dto;public class LoginFormDTO {
+package org.launchcode.javawebdevtechjobsauthentication.models.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class LoginFormDTO {
+
+    @NotNull
+    @NotBlank
+    @Size(min = 3, max = 20, message = "Invalid username. Must be between 3 and 30 characters.")
+    private String username;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 5, max = 20, message = "Invalid password. Must be between 5 and 30 characters.")
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public class RegisterFormDTO extends LoginFormDTO {
+
+        private String verifyPassword;
+
+        public String getVerifyPassword() {
+            return verifyPassword;
+        }
+
+        public void setVerifyPassword(String verifyPassword) {
+            this.verifyPassword = verifyPassword;
+        }
+
+    }
+
 }
